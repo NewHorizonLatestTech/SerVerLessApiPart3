0ea8b859f573838974e5c4c9742e0f4d3144f34e
RanaAmirHussain <105167729+NewHorizonLatestTech@users.noreply.github.com>
2023-03-14 18:23:14 +0500

    a
    a

---
+++ first-appsync-dynamodb-lambda-dataSource/test/appsync-dynamodb-lambda-data_source.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as FirstAppsyncDynamodbLambdaDataSourceStack from '../lib/first-appsync-dynamodb-lambda-data_source-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/appsync-dynamodb-lambda-data_source-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new FirstAppsyncDynamodbLambdaDataSourceStack.FirstAppsyncDynamodbLambdaDataSourceStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
